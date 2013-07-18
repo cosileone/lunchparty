1715341982fb6b95405443b1cd5f81883e563812
Cosimo Leone <cosimo@stackmob.com>
2013-07-18 12:42:29 -0700

    fixed rename changes

--- js/custom-functions.js
+++ js/custom-functions.js
@@ -1,10 +1,11 @@
-$(function() {
+$(function sm_init() {
   StackMob.init({
   publicKey: '7f111c65-e4b7-4c0b-9055-111b77194954',
   apiVersion : 0
+  })
 });
 
-$(function() {
+$(function dragDrop() {
   $( "#token" ).draggable();
   $( ".graphBar" ).droppable({
     drop: function( event, ui ) {
