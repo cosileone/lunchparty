665473975b39409a6619f4fc306ae11bc4c3b480
cosileone <cosi.leone@icloud.com>
2015-03-11 09:36:39 +0000

    move js files

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-$(function sm_init() {
-  StackMob.init({
-  publicKey: '7f111c65-e4b7-4c0b-9055-111b77194954',
-  apiVersion : 0
-  })
-});
-
 $(function dragDrop() {
   $( "#token" ).draggable();
   $( ".graphBar" ).droppable({
